ce9f3fe5061f2667f5ec472c7f61cfadff46ec34
Hosam <hossamnasser938@gmail.com>
2018-10-18 12:26:44 +0200

    Include ViewModel and Api Modules in AppModule

--- app/src/main/java/com/example/android/quizzy/AppModule.java
+++ app/src/main/java/com/example/android/quizzy/AppModule.java
@@ -3,13 +3,16 @@ package com.example.android.quizzy;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.android.quizzy.api.impl.ApiLayerModule;
+import com.example.android.quizzy.viewModel.impl.ViewModelLayerModule;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 
 //TODO:include components modules
-@Module()
+@Module(includes = {ApiLayerModule.class, ViewModelLayerModule.class})
 public class AppModule {
 
     private Application app;
