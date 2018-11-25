4380d2ece9ca8c6c3d10860f5fa8f8e5d3ceb778
Hosam <hossamnasser938@gmail.com>
2018-11-26 01:53:05 +0200

    Check getting number from login/register process

--- app/src/main/java/com/example/android/quizzy/activity/MainActivity.kt
+++ app/src/main/java/com/example/android/quizzy/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.android.quizzy.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.android.quizzy.R
+import com.example.android.quizzy.util.Constants
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,28 @@ class MainActivity : AppCompatActivity() {
             else{
                 val intent = Intent(applicationContext, WalkThroughActivty::class.java)
                 startActivity(intent)
+            }
+        }
+        else {
+            //Check getting number from login/register
+            var isTeacher: Boolean? = null
+            var gotNumber: String? = null
+
+            if (intent.extras.containsKey(Constants.TELEPHONE_NUMBER_KEY)) {
+                isTeacher = true
+                gotNumber = intent.extras[Constants.TELEPHONE_NUMBER_KEY] as String
+            }
+
+            if (intent.extras.containsKey(Constants.TEACHER_TELEPHONE_NUMBER_KEY)) {
+                isTeacher = false
+                gotNumber = intent.extras[Constants.TELEPHONE_NUMBER_KEY] as String
+            }
+
+            if(isTeacher != null){
+                Toast.makeText(this, "Got number = " + gotNumber + " and isTeacher : " + isTeacher, Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Error getting number", Toast.LENGTH_SHORT).show()
             }
         }
     }
