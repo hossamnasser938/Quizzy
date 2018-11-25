1d6bb3a867ecdc39b15dd6f06150a51274505ad1
Hosam <hossamnasser938@gmail.com>
2018-11-25 15:29:03 +0200

    The other library works

--- app/src/main/java/com/example/android/quizzy/activity/IntroActivity.java
+++ app/src/main/java/com/example/android/quizzy/activity/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.quizzy.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,14 +13,22 @@ import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class IntroActivity extends AppIntro {
 
-    AppIntroFragment page1 = AppIntroFragment.newInstance("No paper", "Quizzes can be done with no paper", R.drawable.teacher, getResources().getColor(R.color.colorPrimary));
-    AppIntroFragment page2 = AppIntroFragment.newInstance("Digitize your work", "Quizzes can be digitalized through mobile application", R.drawable.teacher, getResources().getColor(R.color.colorPrimary));
-    AppIntroFragment page3 = AppIntroFragment.newInstance("Keep in touch", "Keep connected with your students anywhere, and anytime", R.drawable.teacher, getResources().getColor(R.color.colorPrimary));
-    AppIntroFragment page4 = AppIntroFragment.newInstance("Promote the process", "Generate reports on students' attitudes to promote the learning process", R.drawable.teacher, getResources().getColor(R.color.colorPrimary));
+    Resources resources;
 
+    AppIntroFragment page1;
+    AppIntroFragment page2;
+    AppIntroFragment page3;
+    AppIntroFragment page4;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        resources = this.getResources();
+        page1 = AppIntroFragment.newInstance("No paper", "Quizzes can be done with no paper", R.drawable.teacher, resources.getColor(R.color.colorPrimary));
+        page2 = AppIntroFragment.newInstance("Digitize your work", "Quizzes can be digitalized through mobile application", R.drawable.teacher, resources.getColor(R.color.colorPrimary));
+        page3 = AppIntroFragment.newInstance("Keep in touch", "Keep connected with your students anywhere, and anytime", R.drawable.teacher, resources.getColor(R.color.colorPrimary));
+        page4 = AppIntroFragment.newInstance("Promote the process", "Generate reports on students' attitudes to promote the learning process", R.drawable.teacher, resources.getColor(R.color.colorPrimary));
+
 
         //Add pages
         addSlide(page1);
